9fc2d942b4ea1a61ba6e709d992710276d7ba36e
BasantJoshi6363 <basantjoshi6363@gmail.com>
2025-08-17 12:18:42 +0545

    fix: correct route definition for Thank You page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/projects'  element={<Projects/>}></Route>
         <Route path='/' index  element={<Home/>}></Route>
-        <Route path='/thankyou' index  element={<ThankYou/>}></Route>
+        <Route path='/thankyou'  element={<ThankYou/>}></Route>
         <Route path='*' index  element={<h3 className='text-white text-center mt-[50vh]'> 404 Page Not Found</h3>}></Route>
       </Routes>
     </div>
